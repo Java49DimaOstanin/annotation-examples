9a4364a71dfa448609e0a1914930ed85020a0b4b
DimaOstanin <xnadab@gmail.com>
2023-12-03 23:14:31 +0200

    first commit

---
+++ src/main/java/telran/annotation/examples/TestClass.java
@@ -0,0 +1,20 @@
+package telran.annotation.examples;
+
+import telran.test.annotation.Test;
+
+public class TestClass {
+private void f1(int b) {
+	System.out.println("test f1 passed " + b);
+}
+    void f1() {
+	System.out.println("test f1 faled ");
+}
+@Test
+    void test1() {
+	f1(10);
+}
+@Test
+	void test() {
+	f1();
+	}
+}
